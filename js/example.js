c2cfa55d7f6bcaee094cc5c80dc88273a7bd0ecb
NathanLeysGit <nathanley.personal@gmail.com>
2019-11-20 22:12:16 -0600

    Cleaned up examples

--- js/example.js
+++ js/example.js
@@ -1,16 +1,17 @@
+//Variables----------
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
-//Rectangles
+//Rectangles---------
 c.fillStyle = "rgba(255, 0, 0, 0.6)";
 c.fillRect(100, 100, 100, 100);
 c.fillRect(500, 100, 100, 100);
 
 
-//Lines
+//Lines--------------
 c.strokeStyle = "rgba(255, 0, 0, 0.3)";
 c.beginPath();
 c.moveTo(200, 100);
@@ -25,13 +26,14 @@ c.lineTo(500, 200);
 c.stroke();
 
 
-//Arcs and Circles
+//Arcs and Circles---
 c.beginPath();
 c.arc(350, 300, 40, 0, Math.PI * 2, false);
 c.stroke();
 
 
-//Printing multiple rectangles
+
+//Multiple rectangles
 for(var i=0; i<40; i++) {
   var x = Math.random() * window.innerWidth;
   var y = Math.random() * window.innerHeight;
@@ -39,7 +41,8 @@ for(var i=0; i<40; i++) {
   c.fillRect(x, y, 50, 50);
 }
 
-//Printing multiple lines
+
+//Multiple lines-----
 for(var i=0; i<160; i++) {
   var x = Math.random() * window.innerWidth;
   var y = Math.random() * window.innerHeight;
@@ -50,7 +53,8 @@ for(var i=0; i<160; i++) {
   c.stroke();
 }
 
-//Printing multiple circles
+
+//Multiple circles---
 for(var i=0; i<40; i++) {
   var x = Math.random() * window.innerWidth;
   var y = Math.random() * window.innerHeight;
